9f0b337d6ae81a52d846e0c254adca865162bbe7
JavierNougues <73NO41579197@campus.economicas.uba.ar>
2023-04-03 11:20:55 -0300

    Clase Contacto terminada

---
+++ CAI_2_3_2023-POO-Agenda/Validacion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAI_2_3_2023_POO_Agenda
+{
+    internal class Validacion
+    {
+        internal static int PedirInt(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
