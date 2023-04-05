878c4b2da1033515e9794b5bbf9f5b2737532465
JavierNougues <73NO41579197@campus.economicas.uba.ar>
2023-04-05 10:11:28 -0300

    Clases codificadas seún especificado. Inicio de codificado de Program.

---
+++ CAI_2_3_2023-POO-Agenda-Intento2/Validacion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAI_2_3_2023_POO_Agenda_Intento2
+{
+    internal class Validacion
+    {
+        internal static int Menu(string v1, int v2, int v3)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
