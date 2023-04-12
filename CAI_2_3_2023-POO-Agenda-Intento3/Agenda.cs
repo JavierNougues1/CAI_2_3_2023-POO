bc321c57fbb93e2ef06b8c92fd5463c852ad3d67
JavierNougues <73NO41579197@campus.economicas.uba.ar>
2023-04-12 11:50:52 -0300

    Clase Agenda. Propiedades y constructor de Contacto.

---
+++ CAI_2_3_2023-POO-Agenda-Intento3/Agenda.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_2_3_2023_POO_Agenda_Intento3
+{
+    internal class Agenda
+    {
+        private string _nombre { get; set; }
+        private string _tipo { get; set; }
+        private int _cantMaxContactos { get; set; }
+        private List<Contacto> _contactos = new List<Contacto>();
+
+        public Agenda(string nombre, string tipo, int cantMaxContactos, List<Contacto> contactos)
+        {
+            _nombre = nombre;
+            _tipo = tipo;
+            _cantMaxContactos = cantMaxContactos;
+            _contactos = contactos;
+        }
+
+        public void AgregarContacto(int codContacto, string nombreContacto, string apellidoContacto, string telefonoContacto, string direccionContacto, DateTime fechaNacContacto, int llamadasContacto)
+        {
+            if (_contactos.Count < _cantMaxContactos)
+            {
+                _contactos.Add(new Contacto(codContacto, nombreContacto, apellidoContacto, telefonoContacto, direccionContacto, fechaNacContacto, llamadasContacto));
+            }
+            else { throw new Exception("Cantidad máxima de contactos alcanzada."); }
+        }
+        public void EliminarContacto(int codContacto)
+        {
+            if (_contactos != null || _contactos.Count == 0)
+            {
+                throw new Exception("La agenda esta vacía.");
+            }
+            foreach (var c in _contactos)
+            {
+                if (c._codContacto == codContacto)
+                {
+                    _contactos.Remove(c);
+                }
+                else
+                {
+                    throw new Exception("No hay un contacto registrado bajo el código ingresado.");
+                }
+                break;
+            }
+        }
+        public Contacto TraerContactoFrecuente()
+        {
+            Contacto contactoFrecuente = null;
+            int cantLlamadas = 0;
+            if (_contactos != null || _contactos.Count == 0)
+            {
+                throw new Exception("La agenda esta vacía.");
+            }
+            foreach (var c in _contactos)
+            {
+                if (c._llamadasContacto > cantLlamadas)
+                {
+                    cantLlamadas = c._llamadasContacto;
+                    contactoFrecuente = c;
+                }
+            }
+            if (cantLlamadas == 0)
+            {
+                throw new Exception("No posee llamadas registradas para niguno de sus contactos.");
+            }
+            return contactoFrecuente;
+        }
+    }
+}
---
+++ CAI_2_3_2023-POO-Agenda-Intento3/Contacto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CAI_2_3_2023_POO_Agenda_Intento3
+{
+    internal class Contacto
+    {
+        public int _codContacto { get; set; }
+        public string _nombreContacto { get; set; }
+        public string _apellidoContacto { get; set; }
+        public string _telefonoContacto { get; set; }
+        public string _direccionContacto { get; set; }
+        public DateTime _fechaNacContacto { get; set; }
+        public int _llamadasContacto { get; set; }
+
+        public Contacto(int codContacto, string nombreContacto, string apellidoContacto, string telefonoContacto, string direccionContacto, DateTime fechaNacContacto, int llamadasContacto)
+        {
+            _codContacto = codContacto;
+            _nombreContacto = nombreContacto;
+            _apellidoContacto = apellidoContacto;
+            _telefonoContacto = telefonoContacto;
+            _direccionContacto = direccionContacto;
+            _fechaNacContacto = fechaNacContacto;
+            _llamadasContacto = llamadasContacto;
+
+
+        }
+    }
+}
