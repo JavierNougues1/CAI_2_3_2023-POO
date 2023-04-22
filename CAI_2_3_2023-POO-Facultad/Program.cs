2394765991c83c494adcf17752aa34b5622717ee
JavierNougues <73NO41579197@campus.economicas.uba.ar>
2023-04-22 12:54:44 -0300

    Codificacion de las clases tal cual diagrama. No tiene ningun tipo de validación.

---
+++ CAI_2_3_2023-POO-Facultad/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAI_2_3_2023_POO_Facultad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
